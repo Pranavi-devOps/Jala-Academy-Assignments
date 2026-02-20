cab44f4e88aa1ac9ea1251cdce28522be1b1e5f5
Pranavi-devOps <pranavi312006@gmail.com>
2026-02-20 12:39:22 +0530

    Added Java folder

---
+++ Python Basics/data_types.py
@@ -0,0 +1,11 @@
+a = 10
+b = True
+c = 'P'
+d = 5.5
+e = 10.1234
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
---
+++ Python Basics/local_global_variables.py
@@ -0,0 +1,8 @@
+x = 100
+
+def display():
+    x = 50
+    print(x)
+
+display()
+print(x)
